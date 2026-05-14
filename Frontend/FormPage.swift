a3c01a58097523262ca681769aba3a79a5421259
Amit Raj <amitrj82701@gmail.com>
2026-05-14 21:00:16 +0530

    Make frontend ui

---
+++ Frontend/FormPage.swift
@@ -0,0 +1,18 @@
+//
+//  FormPage.swift
+//  Frontend
+//
+//  Created by Amit Raj on 14/05/26.
+//
+
+import SwiftUI
+
+struct FormPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormPage()
+}
