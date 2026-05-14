6a1a07e4d70902b741254e4d6c26f06e7b5a28f4
Amit Raj <amitrj82701@gmail.com>
2026-05-14 21:01:13 +0530

    Push All the Ui Related Stuff

--- Frontend/ContentView.swift
+++ Frontend/ContentView.swift
@@ -8,16 +8,145 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShow : Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Image("HomePageImage")
+                .resizable()
+                .scaledToFill()
+                . ignoresSafeArea()
+                .offset(x:-20)
+                .overlay(alignment: .bottom){
+                    VStack(alignment: . leading){
+                        Text("Your Daily Store")
+                            . font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundStyle(.white)
+                            .padding(.bottom , 10)
+                        Text("This is our short Desciption. I want it to be crisp and punchy")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            .foregroundStyle(.secondary)
+                        if !isShow {
+                            NavigationLink{
+                                FormPage()
+                            } label: {
+                                VStack{
+                                    Text("Create Account")
+                                        .foregroundStyle(.white)
+                                        .fontWeight(.medium)
+                                        .font(.title2)
+                                }
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(.green, in: .capsule)
+                                
+                            }
+                            .padding(.vertical, 10)
+                            HStack{
+                                Button {
+                                    
+                                } label: {
+                                    VStack{
+                                        Text("Browse the app")
+                                            .tint(.gray)
+                                            .font(.title2)
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical)
+                                    .background(.ultraThinMaterial.opacity(0.3), in:.capsule)
+                                    .tint(.secondary)
+                                }
+                                Button {
+                                    withAnimation {
+                                        isShow.toggle()
+                                    }
+                                } label: {
+                                    VStack{
+                                        Text("Sign In")
+                                            .tint(.gray)
+                                            .font(.title2)
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .background(.ultraThinMaterial.opacity(0.3), in:.capsule)
+                                    .tint(.secondary)
+                                }
+                            }
+                        }else{
+                            VStack{
+                                signInButtons()
+                            }
+                            .transition(.blurReplace)
+                        }
+                    }
+                    .padding(40)
+                    
+                    
+                }
         }
-        .padding()
+    }
+    @ViewBuilder
+    func signInButtons()->some View{
+        Button {
+            
+        } label: {
+            HStack{
+                Image(systemName: "apple.logo")
+                    .foregroundStyle(.black)
+                    .font(.title2)
+                Text("Sign In With Apple")
+                    .font(.title2)
+                    .foregroundStyle(.black)
+                    .fontWeight(.medium)
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.white, in: .capsule)
+        }
+        Button {
+            
+        } label: {
+            HStack{
+                Image("Google")
+                    .resizable()
+                    .frame(width : 28,height: 28)
+                    .foregroundStyle(.black)
+                    .font(.title2)
+                Text("Sign In With Google")
+                    .font(.title2)
+                    .foregroundStyle(.black)
+                    .fontWeight(.medium)
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.white, in: .capsule)
+        }
+        Button {
+            
+        } label: {
+            HStack{
+                
+                Text("Sign In With Phone Number")
+                    .font(.title2)
+                    .foregroundStyle(.black)
+                    .fontWeight(.medium)
+                
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.green, in: .capsule)
+        }
+        Button("Back") {
+            withAnimation {
+                isShow.toggle()
+            }
+            
+        }
+        .tint(.black)
     }
 }
+
 
 #Preview {
     ContentView()
--- Frontend/FormPage.swift
+++ Frontend/FormPage.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct FormPage: View {
+    @State var firstName : String = ""
+    @State var LastName : String? = nil
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            Text("What's Your name ?")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .fontDesign(.rounded)
+            TextField("First Name", text: $firstName)
+                .fontDesign(.rounded)
+                Rectangle()
+                .frame(height: 4)
+                .padding(.bottom, 10)
+            TextField("Last Name", text: $firstName)
+                .fontDesign(.rounded)
+                Rectangle()
+                .frame(height: 4)
+            Text("Last Name is Optional")
+                .foregroundStyle(.secondary)
+                .font(.footnote)
+        }
+        .padding()
+        
     }
 }
 
